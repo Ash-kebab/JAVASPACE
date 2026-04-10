e5c3ba23a57086c7b366c54e61cda59055333263
Ash-kebab <ashishvinod25@gmail.com>
2026-04-10 18:24:40 +0000

    Test

---
+++ Something.java
@@ -0,0 +1,14 @@
+public class Something{
+
+public static void main(String [] args)
+{
+    System.out.println("Hi");
+    System.out.println("Bye");
+    int x = 5; 
+
+    System.out.println(x);
+}
+
+
+
+}
