855942fcd28d93015790837cbd736b4a95a6c4c8
Ash-kebab <ashishvinod25@gmail.com>
2026-04-11 20:58:44 +0000

    Edit 2

--- Testing.java
+++ Testing.java
@@ -3,5 +3,8 @@ public class Testing
     public static void main(String [] args)
     {
         System.out.println("Hello world!");
+        for(int i = 0 ; i < 10 ; i++ )
+            System.out.println("Hello ");
+            System.out.println("wow");
     }
 }
